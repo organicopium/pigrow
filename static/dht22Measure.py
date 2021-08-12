34d0efe5835b7257dbc50105cc24fd85a12d2600
organicopium <organicopium@hotmail.com>
2021-08-12 17:48:46 +0200

    Changed directory structure

---
+++ static/dht22Measure.py
@@ -0,0 +1,54 @@
+import sqlite3
+from sqlite3 import Error
+import time
+import datetime
+from dht.models import DHT22Measure
+
+db = "../db.sqlite3"
+
+def create_table(conn):
+    create_table_query = """ CREATE TABLE IF NOT EXISTS dht_data (
+                            id integer PRIMARY KEY,
+                            humidity real ,
+                            temperature real,
+                            ts text
+                        );"""
+    try:
+        c = conn.cursor()
+        c.execute(create_table_query)
+    except Error as e:
+        print(e)
+
+def create_connection(db):
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    return conn
+
+def insertMeasure(conn, measure):
+    insert_query = ''' INSERT INTO dht_data(humidity, temperature, ts)
+              VALUES(?, ?, ?) '''
+    cur = conn.cursor()
+    cur.execute(insert_query, measure)
+    conn.commit()
+
+def work():
+    conn = create_connection(db)
+    import Adafruit_DHT
+    sensor = Adafruit_DHT.DHT22
+    sensor_pin = 18
+    while conn is not None:
+        create_table(conn)
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+        ts = datetime.datetime.now().timestamp()
+        measure = (humidity, temperature, ts)
+        insertMeasure(conn, measure)
+        print("inserted {}".format(measure))
+        time.sleep(1)
+    print("Database connection does not exist")
+
+if __name__ == '__main__':
+    work()
--- static/dht22_trial.py
+++
@@ -1,8 +0,0 @@
-import Adafruit_DHT
-
-sensor = Adafruit_DHT.DHT22
-sensor_pin = 18
-
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
-    print("humidity: {}, temperature {}".format(humidity, temperature))

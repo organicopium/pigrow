28e22405a07e192fc4e922b40c1ebc69cd65f914
mandeep.kaler <mandeep.kaler@hg.magentalab.it>
2021-08-12 14:47:20 +0200

    dht's index responds with temperature and humidity

--- dht/views.py
+++ dht/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import Adafruit_DHT
+
+
+
+
+def index(request):
+    humidity, temperature = getCurrentMeasures()
+    return HttpResponse("temperature {:.2f}°c, humidity {:.2f}%".format(humidity, temperature))
+
+def getCurrentMeasures():
+    sensor = Adafruit_DHT.DHT22
+    ensor_pin = 18
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+    return humidity, temperature

381986b0b0a5f2e2e52b27884657e73b31362539
organicopium <organicopium@hotmail.com>
2021-08-12 18:16:41 +0200

    Read latest measure id in dht's view

--- dht/views.py
+++ dht/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from dht import measure
-
+from dht.models import DhtData
 
 
 def index(request):
-    #TODO read from db and respond
-    return HttpResponse("temperature {:.2f}°c, humidity {:.2f}%".format(-1, -1))
+    d = DhtData.objects.latest('id')
+    TS_FORMAT = '%Y-%m-%d %H:%M:%S'
+    ts_formatted = datetime.utcfromtimestamp(d.ts).strftime(TIME_FORMAT)
+    return HttpResponse("temperature {:.2f}°c, humidity {:.2f}%, at {}".format(d.temperature, d.humidity, ts_formatted))

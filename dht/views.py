f7ae26aa2f813a57b13aaf9724dec2d1cdc5b892
organicopium <organicopium@hotmail.com>
2021-08-14 18:12:10 +0200

    Visualize temperature in Fahrenheit as well

--- dht/views.py
+++ dht/views.py
@@ -7,4 +7,5 @@ def index(request):
     d = DhtData.objects.latest('id')
     TS_FORMAT = '%Y-%m-%d %H:%M:%S'
     ts_formatted = datetime.utcfromtimestamp(float(d.ts)).strftime(TS_FORMAT)
-    return HttpResponse("{}: temperature {:.2f}°c, humidity {:.2f}%".format(ts_formatted, d.temperature, d.humidity))
+    temperature_f = d.temperature * 1.8 + 32.0
+    return HttpResponse("temperature {:.2f}°c, {:-2f}°F; humidity {:.2f}%, at {}".format(d.temperature, temperature_f, d.humidity, ts_formatted))
